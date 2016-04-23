50269ff83b7532065ded54c0ee62a2bd764a70b5
zhenjiuziran <zhenjiuziran@qq.com>
2016-04-23 23:41:03 +0800

    test create user

--- shell/user_create.js
+++ shell/user_create.js
@@ -2,3 +2,21 @@ var Sequelize = require('sequelize');
 var config_db = require("./config_db");
 
 var sequelize = new Sequelize(config_db.database, config_db.username, config_db.password);
+
+
+var User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+},{
+  timestamps: true,
+  createdAt: "time_create",
+  updatedAt: "time_update",
+  tableName: "users"
+});
+
+User.create({
+  username:"admin",
+  password:"987654"
+}).then(function(user) {
+  console.log(user.get({plain:true}))
+});
