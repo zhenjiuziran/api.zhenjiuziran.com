7088d542bea88514a1d9a3d1c72b818b3b6e1e7c
zhenjiuziran <zhenjiuziran@qq.com>
2016-04-24 14:35:43 +0800

    password use md5 match

--- interface/i_user.js
+++ interface/i_user.js
@@ -1,12 +1,12 @@
 var User = require("./models/user");
-
+var md5 = require("md5");
 var i_user = {
   auth: function(username,password) {
     console.log(username,password);
     User.findOne({
       where: {
         username: username,
-        password: password
+        password: md5(password)
       }
     }).then(function(user) {
       console.log(user);
