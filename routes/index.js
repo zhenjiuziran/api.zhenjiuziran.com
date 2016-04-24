81d68af4b10c6de3a242a79634617498afebc6d2
zhenjiuziran <zhenjiuziran@qq.com>
2016-04-24 14:00:18 +0800

    route add login

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.post('/login/:username/:password',function(req,res,next) {
+  console.log(req.params.username);
+  console.log(req.params.password);
+});
 module.exports = router;
